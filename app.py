231cc4361d540ec0730fa757e0d6ae1b2ab181d7
Frank Kortstiege <frank.kortstiege@gmail.com>
2024-04-04 23:40:47 +0200

    add node name

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
+import os
 from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    node_name = os.getenv('NODE_NAME', 'Unknown')
+    return render_template('index.html', node_name=node_name)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
