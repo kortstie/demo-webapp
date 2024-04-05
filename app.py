a6fa501f7398ec391b0a69613b5c9abadd39f81e
Frank Kortstiege <frank.kortstiege@gmail.com>
2024-04-05 23:49:21 +0200

    more nodes

--- app.py
+++ app.py
@@ -4,10 +4,8 @@ from kubernetes import client, config
 
 app = Flask(__name__)
 
-@app.route('/')
+@@app.route('/')
 def home():
-    node_name = os.getenv('NODE_NAME', 'Unknown')
-    
     config.load_incluster_config()
     
     version_api = client.VersionApi()
@@ -15,15 +13,24 @@ def home():
     k8s_version = k8s_version_info.git_version
     
     core_api = client.CoreV1Api()
-    node_info = core_api.read_node(name=node_name)
-    nodepool_name = node_info.metadata.labels.get('agentpool', 'Unknown')
-    failure_domain_zone = node_info.metadata.labels.get('failure-domain.beta.kubernetes.io/zone', 'Unknown')
-    failure_domain_region = node_info.metadata.labels.get('failure-domain.beta.kubernetes.io/region', 'Unknown')
-    
-    cpu_capacity = node_info.status.capacity.get('cpu', 'Unknown')
-    memory_capacity = node_info.status.capacity.get('memory', 'Unknown')
-    
-    return render_template('index.html', k8s_version=k8s_version, node_name=node_name, nodepool_name=nodepool_name, failure_domain_zone=failure_domain_zone, failure_domain_region=failure_domain_region, cpu_capacity=cpu_capacity, memory_capacity=memory_capacity)    
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    nodes = core_api.list_node().items
 
+    node_data = []
+    for node in nodes:
+        node_name = node.metadata.name
+        nodepool_name = node.metadata.labels.get('agentpool', 'Unknown')
+        failure_domain_zone = node.metadata.labels.get('failure-domain.beta.kubernetes.io/zone', 'Unknown')
+        failure_domain_region = node.metadata.labels.get('failure-domain.beta.kubernetes.io/region', 'Unknown')
+        cpu_capacity = node.status.capacity.get('cpu', 'Unknown')
+        memory_capacity = node.status.capacity.get('memory', 'Unknown')
+
+        node_data.append({
+            'node_name': node_name,
+            'nodepool_name': nodepool_name,
+            'failure_domain_zone': failure_domain_zone,
+            'failure_domain_region': failure_domain_region,
+            'cpu_capacity': cpu_capacity,
+            'memory_capacity': memory_capacity
+        })
+    
+    return render_template('index.html', k8s_version=k8s_version, node_data=node_data)
