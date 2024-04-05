6d8fea5ab52dd90022a47a6ede036e7ff309d719
Frank Kortstiege <frank.kortstiege@gmail.com>
2024-04-05 22:43:14 +0200

    k8s_version

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 import os
 from flask import Flask, render_template
+from kubernetes import client, config
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     node_name = os.getenv('NODE_NAME', 'Unknown')
-    return render_template('index.html', node_name=node_name)
+    config.load_incluster_config()
+    v1 = client.VersionApi()
+    k8s_version = v1.get_code()
+    return render_template('index.html', node_name=node_name, k8s_version=k8s_version)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
