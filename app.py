28ce0282ca72b555827ef3651e72370f9cb08da4
Frank Kortstiege <frank.kortstiege@gmail.com>
2024-04-05 23:03:30 +0200

    more values

--- app.py
+++ app.py
@@ -11,6 +11,11 @@ def home():
     v1 = client.VersionApi()
     k8s_version_info = v1.get_code()
     k8s_version = k8s_version_info.git_version
+    nodepool_name = node_info.metadata.labels.get('agentpool', 'Unknown')
+    failure_domain_zone = node_info.metadata.labels.get('failure-domain.beta.kubernetes.io/zone', 'Unknown')
+    failure_domain_region = node_info.metadata.labels.get('failure-domain.beta.kubernetes.io/region', 'Unknown')
+    
+    return render_template('index.html', node_name=node_name, nodepool_name=nodepool_name, failure_domain_zone=failure_domain_zone, failure_domain_region=failure_domain_region)
     return render_template('index.html', node_name=node_name, k8s_version=k8s_version)
 
 if __name__ == '__main__':
